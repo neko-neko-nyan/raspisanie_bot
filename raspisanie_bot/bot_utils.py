d399d522b8feafa2a30975231986fcf19db3b18e
NekoNekoNyan <pasha2001dpa@ya.ru>
2021-09-18 20:48:05 +0300

    New utils

--- raspisanie_bot/bot_utils.py
+++ raspisanie_bot/bot_utils.py
@@ -1,13 +1,19 @@
+import peewee
+
 from raspisanie_bot import parse_group_name
 from raspisanie_bot.bot_errors import bot_error
 from raspisanie_bot.database import Group, Teacher
 
 
+def group_from_parsed(group):
+    return Group.get_or_none(Group.course == group.course, Group.group == group.group,
+                             Group.subgroup == group.subgroup)
+
+
 def get_group_or_none(text):
     group = parse_group_name(text, only_if_matches=True)
     if group is not None:
-        group = Group.get_or_none(Group.course == group.course, Group.group == group.group,
-                                  Group.subgroup == group.subgroup)
+        group = group_from_parsed(group)
 
     return group
 
@@ -18,6 +24,19 @@ def get_group_or_bot_error(user, text):
         bot_error("INVALID_GROUP", user=user.tg_id, group=text)
 
     return group
+
+
+def teacher_from_parsed(teacher):
+    surname, n, p = teacher
+
+    try:
+        return Teacher.select().where(
+            (Teacher.surname == surname).collate("NOCASE"),
+            Teacher.name.startswith(n).collate("NOCASE"),
+            Teacher.patronymic.startswith(p).collate("NOCASE")
+        ).get()
+    except peewee.DoesNotExist:
+        pass
 
 
 def get_teacher_or_none(text):
